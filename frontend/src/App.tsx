59ddac2330aed998919c22f930bd9d109dd762d4
Josh <joshuabgudgel@gmail.com>
2025-06-23 16:12:58 -0700

    Experimenting with react-spring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import Navbar from "./components/ui/Navbar";
 import Hero from "./components/sections/Hero";
 import Packages from "./components/sections/Packages";
@@ -5,25 +6,33 @@ import Menu from "./components/sections/Menu";
 import BookingModal from "./components/ui/BookingModal";
 import { BookingProvider, useBooking } from "./context/BookingContext";
 import { MenuProvider } from "./context/MenuContext";
-import ParallaxExperiment from "./components/ParallaxExperiment";
+import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
 
 const AppContent = () => {
   const { isModalOpen, selectedPackage, closeModal } = useBooking();
-
+  const parallax = useRef<IParallax>(null!);
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <Hero />
-      <Packages />
-      <BookingModal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        selectedPackage={selectedPackage}
-      />
-      <Menu />
-      <ParallaxExperiment />
-      {/* Other sections will be added here later */}
-    </div>
+    <Parallax ref={parallax} pages={3}>
+      <div className="min-h-screen">
+        <ParallaxLayer offset={0} speed={0}>
+          <Navbar />
+          <Hero />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0}>
+          <Packages />
+          <BookingModal
+            isOpen={isModalOpen}
+            onClose={closeModal}
+            selectedPackage={selectedPackage}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} speed={0}>
+          <Menu />
+        </ParallaxLayer>
+        {/* Other sections will be added here later */}
+      </div>
+    </Parallax>
   );
 };
 
